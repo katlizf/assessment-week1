b1dfcf352489b0e50f5ec9675dc102ed3f1ba045
Katie Anderson <katherine.lizf@gmail.com>
2022-03-25 13:58:49 -0500

    Version 2

--- password.js
+++ password.js
@@ -1,7 +1,7 @@
 console.log("Welcome to the password validator tool")
 console.log("Please enter your password.")
 
-let password = []
+let password = "fdsa5klojoihn6"
 if (password.length >= 10) {
     console.log("Strong password!")
 } else {
